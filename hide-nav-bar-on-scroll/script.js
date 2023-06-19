6e9e294227859a61021b48b104a969c70c1110b2
Syed Tanveer <15954485+syedtanveer@users.noreply.github.com>
2023-06-19 09:12:28 +0530

    feat(components): add new components - slidedown navbar on scroll - hide navbar on scroll

--- hide-nav-bar-on-scroll/script.js
+++ hide-nav-bar-on-scroll/script.js
@@ -1,10 +1,11 @@
 const navbar = document.querySelector('.navbar');
 
+// find initial pageYOffset
 let prevPageScroll = window.scrollY;
 window.onscroll = function onScroll() {
     const currPageScroll = window.scrollY;
     // prevPageScroll will be greater than currPageScroll only if we are scrolling up
-    if( prevPageScroll < currPageScroll ) {
+    if( prevPageScroll > currPageScroll ) {
         navbar.style.top = '0';
     } else {
         navbar.style.top = '-45px';
