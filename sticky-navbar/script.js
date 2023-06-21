72dbbf702cc4748574688fa9dc3de6d792585e6a
Syed Tanveer <15954485+syedtanveer@users.noreply.github.com>
2023-06-21 09:01:13 +0530

    feat: add new component - sticky navbar - hover dropdown

---
+++ sticky-navbar/script.js
@@ -0,0 +1,10 @@
+// const navbar = document.querySelector('.navbar');
+
+// const navbarOffsetTop = navbar.offsetTop;
+// window.onscroll = function() {
+//     if(window.scrollY >= navbarOffsetTop) {
+//         navbar.classList.add('sticky');
+//     } else {
+//         navbar.classList.remove('sticky');
+//     }
+// }
